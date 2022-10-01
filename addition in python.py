70836519c54ef4b4b737bc8108a3d1aa979fc7e5
Rishabh <devileatspie@gmail.com>
2022-10-01 17:32:32 +0530

    added new file

---
+++ addition in python.py
@@ -0,0 +1,4 @@
+#This is a programme to add numbers using python 
+x=int(input( ' Enter the first no you want to add: '))
+y=int(input( ' Enter the second no you want to add: '))
+print('The sum of the two numbers you added is : ', x+y)
--- code_hello.py
+++ code_hello.py
@@ -1,3 +1,4 @@
 print ("hello, world. how are you doing?")
 x=input ("what is your name?")
 print (x,"!!")
+print( 'be happy')
